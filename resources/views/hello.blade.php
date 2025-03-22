6754a86b82f926a46d44c8105d9ac6419d455674
Risa Goto <omusubi5656dup@gmail.com>
2025-03-22 14:35:49 +0900

    add:ルート、ビュー設定

---
+++ resources/views/hello.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello world</title>
+</head>
+<body>
+    <h1 style="color: blue; font-size: 3rem; font-weight: bold;">hello world</h1>
+    <p>ようこそ{{ $name }}さん</p>
+    <p>本コースでは{{ $course }}を学びます</p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/hello-world',function() {
+//     return 'hello world';
+// });
+
+Route::get('/hello-world' ,fn() => view(view:'hello_world'));
+Route::get('/hello', fn() => view('hello',[
+    'name' => '山田太郎',
+    'course' => 'Laravel',
+]));
